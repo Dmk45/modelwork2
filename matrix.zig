bdb2bd191877a186f6814aeca887eebeed0114c9
taw.wimber@gmail.com <taw.wimber@gmail.com>
2026-02-22 20:41:25 +0300

    testing zig

--- matrix.zig
+++ matrix.zig
@@ -1,8 +1,12 @@
 const std = @import("std");
-pub fn brew() !void {
-    std.debug.print("Hello, world!\n", .{});
+pub fn brew() ![5]i32 {
+    var mut_array_of_int: [5]i32 = [5]i32{ 1, 2, 3, 4, 5 };
+    mut_array_of_int[0] = 10;
+    std.debug.print("mut_array_of_int: {any}\n", .{mut_array_of_int[0]});
+    return mut_array_of_int;
 }
 
 pub fn main() !void {
-    brew();
+    const x = try brew();
+    std.debug.print("mut_array_of_int: {any}\n", .{x});
 }
